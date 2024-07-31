f376f2d7dec48dacad6119c186c1e0a3d1ae9676
Van-Dong <dongnv4724@gmail.com>
2024-07-31 20:54:31 +0700

    Create repository

---
+++ backend/src/main/java/com/dongnv/movie_website/MovieWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.dongnv.movie_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWebsiteApplication.class, args);
+	}
+
+}
